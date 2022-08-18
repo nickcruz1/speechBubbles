d6d250778799a4eb1e1ea7576fc234389cfc4d6e
Nick Cruz <nickcruz920@gmail.com>
2022-08-18 13:56:01 -0400

    Added 2 short functions to delete

---
+++ index.js
@@ -0,0 +1,11 @@
+function deleteJohn() {
+ let john = document.querySelector(".evenBubble");
+  
+  john.style.display = "none";
+}
+
+function deleteJane() {
+ let jane = document.querySelector(".oddBubble");
+  
+  jane.style.display = "none";
+}
